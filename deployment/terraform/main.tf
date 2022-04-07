fdb0d18ffa59459b76dd28837c99dd6e9491fbb6
Ophelie Le Mentec <olementec@microsoft.com>
2022-04-07 18:08:45 +0200

    Added participant to dns name.

--- deployment/terraform/main.tf
+++ deployment/terraform/main.tf
@@ -23,11 +23,11 @@ data "azurerm_container_registry" "registry" {
 }
 
 resource "azurerm_container_group" "edc" {
-  name                = "${var.prefix}-edc"
+  name                = "${var.prefix}-${var.participant_name}-edc"
   location            = var.location
   resource_group_name = azurerm_resource_group.participantresourcegroup.name
   ip_address_type     = "Public"
-  dns_name_label      = "${var.prefix}-agera-mvd"
+  dns_name_label      = "${var.prefix}-${var.participant_name}-edc-mvd"
   os_type             = "Linux"
 
   image_registry_credential {
