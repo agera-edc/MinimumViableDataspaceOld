705a04dfbd30510fdf423c4061ef55c938849fd7
Ophelie Le Mentec <olementec@microsoft.com>
2022-04-07 17:42:10 +0200

    Fix

--- deployment/terraform/main.tf
+++ deployment/terraform/main.tf
@@ -25,7 +25,7 @@ data "azurerm_container_registry" "registry" {
 resource "azurerm_container_group" "edc" {
   name                = "${var.prefix}-edc"
   location            = var.location
-  resource_group_name = azurerm_resource_group.participantresourcegroup
+  resource_group_name = azurerm_resource_group.participantresourcegroup.name
   ip_address_type     = "Public"
   dns_name_label      = "${var.prefix}-agera-mvd"
   os_type             = "Linux"
