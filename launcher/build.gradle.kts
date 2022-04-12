feb19188c62548506dc9eb600d5e3fd824638d57
Ophelie Le Mentec <olementec@microsoft.com>
2022-04-12 15:13:24 +0200

    Revert "Removed line excluding poms."
    This reverts commit c0217cd9e3ff6a18af37beda55b9e9a768574056.

--- launcher/build.gradle.kts
+++ launcher/build.gradle.kts
@@ -42,6 +42,7 @@ application {
 }
 
 tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
+    exclude("**/pom.properties", "**/pom.xm")
     mergeServiceFiles()
     archiveFileName.set("dataspaceconnector-basic.jar")
 }
