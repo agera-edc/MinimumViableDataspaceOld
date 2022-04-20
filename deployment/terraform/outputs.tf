532004ce2d75560c8b71d2539a8a0007caa87425
Ophelie Le Mentec <olementec@microsoft.com>
2022-04-20 15:06:28 +0200

    Revert "Check that did id contains the did host."
    This reverts commit c3f648e7437a77742475857ba35dd100c904d245.

--- deployment/terraform/outputs.tf
+++ deployment/terraform/outputs.tf
@@ -7,9 +7,5 @@ output "assets_storage_account" {
 }
 
 output "did_endpoint" {
-  value = length(azurerm_storage_blob.did) > 0 ? "${azurerm_storage_account.did.primary_web_endpoint}/${azurerm_storage_blob.did[0].name}" : null
-}
-
-output "did_host" {
-  value = length(azurerm_storage_blob.did) > 0 ? azurerm_storage_account.did.primary_blob_host : null
+  value = length(azurerm_storage_blob.did) > 0 ? "${azurerm_storage_account.did.primary_web_endpoint}${azurerm_storage_blob.did[0].name}" : null
 }
