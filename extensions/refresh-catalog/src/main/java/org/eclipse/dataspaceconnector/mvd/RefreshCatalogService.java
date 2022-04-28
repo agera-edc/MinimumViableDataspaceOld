44df8a106ce78c1fa80102009985d536b40b2147
Alexandre Gattiker <algattik@microsoft.com>
2022-04-28 08:37:34 +0200

    ext

--- extensions/refresh-catalog/src/main/java/org/eclipse/dataspaceconnector/mvd/RefreshCatalogService.java
+++ extensions/refresh-catalog/src/main/java/org/eclipse/dataspaceconnector/mvd/RefreshCatalogService.java
@@ -1,20 +1,16 @@
 package org.eclipse.dataspaceconnector.mvd;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.eclipse.dataspaceconnector.catalog.spi.FederatedCacheNode;
 import org.eclipse.dataspaceconnector.catalog.spi.FederatedCacheNodeDirectory;
 import org.eclipse.dataspaceconnector.spi.EdcException;
 import org.eclipse.dataspaceconnector.spi.monitor.Monitor;
+import org.eclipse.dataspaceconnector.spi.types.TypeManager;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.Objects;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 /**
  *
@@ -24,10 +20,9 @@ class RefreshCatalogService {
     private final Path nodeJsonDir;
     private final String nodeJsonPrefix;
     private final Monitor monitor;
-    private final ScheduledExecutorService executor;
-    private final ObjectMapper mapper;
+    private final TypeManager typeManager;
 
-    RefreshCatalogService(FederatedCacheNodeDirectory nodeDirectory, Path nodeJsonDir, String nodeJsonPrefix, Monitor monitor) {
+    RefreshCatalogService(FederatedCacheNodeDirectory nodeDirectory, Path nodeJsonDir, String nodeJsonPrefix, Monitor monitor, TypeManager typeManager) {
         if (!nodeJsonDir.toFile().isDirectory()) {
             throw new EdcException(nodeJsonDir + " should be a directory");
         }
@@ -35,28 +30,17 @@ class RefreshCatalogService {
         this.nodeJsonDir = nodeJsonDir;
         this.nodeJsonPrefix = nodeJsonPrefix;
         this.monitor = monitor;
-        this.executor = Executors.newSingleThreadScheduledExecutor();
-        mapper = new ObjectMapper();
-        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        this.typeManager = typeManager;
     }
 
-    void start() {
-        saveNodeEntries();
-        executor.scheduleWithFixedDelay(this::saveNodeEntries, 5, 5, TimeUnit.SECONDS);
-    }
-
-    void shutdown() {
-        executor.shutdown();
-    }
-
-    private void saveNodeEntries() {
+    void saveNodeEntries() {
         try {
             monitor.info("Refreshing catalog");
             var files = Files.find(nodeJsonDir, 1,
                     (path, attrs) -> path.toFile().getName().startsWith(nodeJsonPrefix));
             List<FederatedCacheNode> existingNodes = nodeDirectory.getAll();
             files
-                    .map(this::getFileFederatedCacheNodeFunction)
+                    .map(this::parseFederatedCacheNode)
                     .filter(n -> existingNodes.stream().noneMatch(m -> Objects.equals(m.getName(), n.getName())))
                     .forEach(this::insertNode);
         } catch (IOException e) {
@@ -64,9 +48,9 @@ class RefreshCatalogService {
         }
     }
 
-    private FederatedCacheNode getFileFederatedCacheNodeFunction(Path file) {
+    private FederatedCacheNode parseFederatedCacheNode(Path path) {
         try {
-            return mapper.readValue(file.toFile(), FederatedCacheNode.class);
+            return typeManager.readValue(Files.readString(path), FederatedCacheNode.class);
         } catch (IOException e) {
             throw new EdcException(e);
         }
--- extensions/refresh-catalog/src/test/java/org/eclipse/dataspaceconnector/mvd/RefreshCatalogServiceTest.java
+++ extensions/refresh-catalog/src/test/java/org/eclipse/dataspaceconnector/mvd/RefreshCatalogServiceTest.java
@@ -3,17 +3,14 @@ package org.eclipse.dataspaceconnector.mvd;
 import org.eclipse.dataspaceconnector.catalog.spi.FederatedCacheNode;
 import org.eclipse.dataspaceconnector.catalog.spi.FederatedCacheNodeDirectory;
 import org.eclipse.dataspaceconnector.spi.monitor.ConsoleMonitor;
+import org.eclipse.dataspaceconnector.spi.types.TypeManager;
 import org.junit.jupiter.api.Test;
 
 import java.nio.file.Path;
 import java.util.List;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.argThat;
-import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -22,25 +19,19 @@ import static org.mockito.Mockito.when;
 class RefreshCatalogServiceTest {
 
     @Test
-    void start() throws InterruptedException {
+    void saveNodeEntries() {
         var directory = mock(FederatedCacheNodeDirectory.class);
-        var nodeJsonDir = Path.of(getClass().getClassLoader().getResource("24-node1.json").getPath()).getParent();
+        var sampleFile = getClass().getClassLoader().getResource("24-node1.json");
+        assertThat(sampleFile).isNotNull();
+        var nodeJsonDir = Path.of(sampleFile.getPath()).getParent();
         var nodeJsonPrefix = "24-";
         var monitor = new ConsoleMonitor();
-        var extension = new RefreshCatalogService(directory, nodeJsonDir, nodeJsonPrefix, monitor);
+        var typeManager = new TypeManager();
+        var extension = new RefreshCatalogService(directory, nodeJsonDir, nodeJsonPrefix, monitor, typeManager);
 
         when(directory.getAll()).thenReturn(List.of(new FederatedCacheNode("node24-2", "", List.of(""))));
 
-        var latch = new CountDownLatch(2);
-
-        doAnswer(i -> {
-            latch.countDown();
-            return null;
-        }).when(directory).insert(any());
-
-        extension.start();
-
-        assertThat(latch.await(1, TimeUnit.MINUTES)).isTrue();
+        extension.saveNodeEntries();
 
         verify(directory, times(1)).insert(argThat(n -> "node24-1".equals(n.getName())));
         verify(directory, times(0)).insert(argThat(n -> "node24-2".equals(n.getName())));
