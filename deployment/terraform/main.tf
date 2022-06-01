b5906df2428f56b1a2d39a135df6efc4ba367ade
Izabela Kulakowska <ikulakowska@microsoft.com>
2022-06-01 16:15:49 +0200

    Changes after review

--- deployment/terraform/main.tf
+++ deployment/terraform/main.tf
@@ -267,6 +267,14 @@ resource "azurerm_storage_blob" "testfile" {
   source                 = "sample-data/text-document.txt"
 }
 
+resource "azurerm_storage_blob" "testfile2" {
+  name                   = "text-document-2.txt"
+  storage_account_name   = azurerm_storage_account.assets.name
+  storage_container_name = azurerm_storage_container.assets_container.name
+  type                   = "Block"
+  source                 = "sample-data/text-document.txt"
+}
+
 resource "azurerm_key_vault_secret" "asset_storage_key" {
   name         = "${azurerm_storage_account.assets.name}-key1"
   value        = azurerm_storage_account.assets.primary_access_key
--- extensions/policies/src/main/java/org/eclipse/dataspaceconnector/mvd/SeedPoliciesExtension.java
+++ extensions/policies/src/main/java/org/eclipse/dataspaceconnector/mvd/SeedPoliciesExtension.java
@@ -8,11 +8,22 @@ import org.eclipse.dataspaceconnector.spi.system.ServiceExtensionContext;
 
 import static org.eclipse.dataspaceconnector.ids.spi.policy.IdsPolicyExpressions.ABS_SPATIAL_POSITION;
 
+/**
+ * Extension to initialize the policies.
+ */
 public class SeedPoliciesExtension implements ServiceExtension {
 
+    /**
+     * Registry that manages rule bindings to policy scopes.
+     */
     @Inject
     private RuleBindingRegistry ruleBindingRegistry;
 
+    /**
+     * Initializes the extension by binding the policies to the rule binding registry.
+     *
+     * @param context service extension context.
+     */
     @Override
     public void initialize(ServiceExtensionContext context) {
         ruleBindingRegistry.bind("USE", ContractDefinitionService.NEGOTIATION_SCOPE);
