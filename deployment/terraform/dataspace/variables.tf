9368af1fbc7e68776a256cd8790812c1c868992a
Alexandre Gattiker <algattik@microsoft.com>
2022-06-04 04:32:41 +0200

    .

--- deployment/terraform/dataspace/variables.tf
+++ deployment/terraform/dataspace/variables.tf
@@ -10,3 +10,36 @@ variable "location" {
 variable "resource_group" {
   default = "test-dataspace"
 }
+
+variable "runtime_image" {
+  description = "Image name of the EDC Connector to deploy"
+}
+
+variable "acr_name" {
+  default = "ageramvd"
+}
+
+variable "acr_resource_group" {
+  default = "agera-mvd-common"
+}
+
+variable "container_cpu" {
+  default = "0.5"
+}
+
+variable "container_memory" {
+  default = "1.5"
+}
+
+variable "registry_resource_group" {
+  description = "resource group of the registry JSON documents file share storage account"
+}
+
+variable "registry_storage_account" {
+  description = "name of the registry JSON documents file share storage account"
+}
+
+variable "registry_share" {
+  description = "name of the registry JSON documents file share"
+}
+
