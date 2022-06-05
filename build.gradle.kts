a1bea39855184672f4ba192c49d515aecb44978e
Paul Latzelsperger <43503240+paullatzelsperger@users.noreply.github.com>
2022-06-05 09:55:43 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,10 @@ allprojects {
     repositories {
         mavenCentral()
         mavenLocal()
+        // prepare for switch to using pinned snapshot builds
+        maven {
++            url = uri("https://oss.sonatype.org/content/repositories/snapshots/")
++        }
         maven {
             url = uri("https://maven.iais.fraunhofer.de/artifactory/eis-ids-public/")
         }
