a7ba3e89d12e5884cd1ce4d4aa31d86877305dd9
Ophelie Le Mentec <17216799+ouphi@users.noreply.github.com>
2022-06-07 09:47:56 +0200

    Removed + breaking the build.

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,8 +9,8 @@ allprojects {
         mavenLocal()
         // prepare for switch to using pinned snapshot builds
         maven {
-+            url = uri("https://oss.sonatype.org/content/repositories/snapshots/")
-+        }
+            url = uri("https://oss.sonatype.org/content/repositories/snapshots/")
+         }
         maven {
             url = uri("https://maven.iais.fraunhofer.de/artifactory/eis-ids-public/")
         }
