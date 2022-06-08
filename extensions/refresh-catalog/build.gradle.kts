9639857609e75429318c4f469efa31c8209be0ad
Alexandre Gattiker <algattik@microsoft.com>
2022-06-08 14:27:04 +0200

    Build registration service client

--- extensions/refresh-catalog/build.gradle.kts
+++ extensions/refresh-catalog/build.gradle.kts
@@ -4,12 +4,16 @@ plugins {
 
 val edcVersion: String by project
 val edcGroup: String by project
+val registrationServiceVersion: String by project
+val registrationServiceGroup: String by project
 val jupiterVersion: String by project
 val mockitoVersion: String by project
 val assertj: String by project
 
 dependencies {
     implementation("${edcGroup}:federated-catalog-spi:${edcVersion}")
+
+    implementation("${registrationServiceGroup}:registration-service-client:${registrationServiceVersion}")
 
     testImplementation("org.assertj:assertj-core:${assertj}")
     testImplementation("org.junit.jupiter:junit-jupiter-api:${jupiterVersion}")
